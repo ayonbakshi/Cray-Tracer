809452cb8b1ca28cc8a08e9b1cc1e7218900c3be
Ayon Bakshi <ayon.bakshi@gmail.com>
2019-10-25 23:10:55 -0400

    ray cast working

---
+++ raytracer.cpp
@@ -0,0 +1,203 @@
+#include <math.h>
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+#include <fstream>
+
+constexpr double INF = 1e10;
+
+struct Color{
+    std::array<int, 3> rgb;
+    Color(): rgb{{0,0,0}} {}
+    Color(int r, int g, int b): rgb{{r,g,b}} {}
+    int &r() { return rgb[0]; }
+    int &g() { return rgb[1]; }
+    int &b() { return rgb[2]; }
+};
+
+const Color red{255, 0, 0};
+const Color green{0, 255, 0};
+const Color blue{0, 0, 255};
+const Color black{0, 0, 0};
+const Color white{255, 255, 255};
+const Color background{160, 255, 253};
+
+template<typename T>
+struct Vec3 {
+    std::array<T, 3> p;
+    Vec3(): p{{0,0,0}} {}
+    Vec3(T x, T y, T z): p{{x,y,z}} {}
+    Vec3(std::array<T, 3> &arr): p{arr} {}
+    T &x() { return p[0]; }
+    T &y() { return p[1]; }
+    T &z() { return p[2]; }
+    void print(){ std::cout << x() << ":" << y() << ":" << z() << std::endl;}
+
+    T norm() const {
+        T sum;
+        for(const T &t : p) sum += t * t;
+        return sqrt(sum);
+    }
+    void normalize() {
+        T mag = norm();
+        if(mag != 0)
+            for(T &t : p) t /= mag;
+    }
+
+    T dot(const Vec3<T> &other) { return p[0] * other.p[0] + p[1] * other.p[1] + p[2] * other.p[2]; }
+
+    Vec3<T> operator*(const T &s) const { return Vec3<T>{s * p[0], s * p[1], s * p[2]}; }
+    Vec3<T> operator+(const Vec3<T> &other) const {return Vec3<T>{p[0] + other.p[0], p[1] + other.p[1], p[2] + other.p[2]}; }
+    Vec3<T> operator-(const Vec3<T> &other) const {return Vec3<T>{p[0] - other.p[0], p[1] - other.p[1], p[2] - other.p[2]}; }
+};
+
+typedef Vec3<double> Vec3d;
+
+struct Material{
+    Color color;
+};
+
+
+// TODO add ray_intersect fn for different objects. Return dist, hit loc and surface normal
+struct Sphere {
+    Vec3d center;
+    double radius;
+    Material material;
+
+    Sphere(Vec3d center, double radius, Material material):
+        center{center}, radius{radius}, material{material} {}
+
+    bool ray_intersection(const Vec3d &ray_orig, const Vec3d &ray_dir, double &t0, double &t1) const {
+        Vec3d L = center - ray_orig; 
+        double tca = L.dot(ray_dir); 
+        // if (tca < 0) return false;
+        double d2 = L.dot(L) - tca * tca; 
+        if (d2 > radius * radius) return false; 
+        double thc = sqrt(radius * radius - d2); 
+        t0 = tca - thc; 
+        t1 = tca + thc; 
+        
+
+        if (t0 > t1) std::swap(t0, t1);
+
+        if (t0 < 1e-6) { 
+            t0 = t1; // if t0 is negative, let's use t1 instead 
+            if (t0 < 1e-6) return false; // both t0 and t1 are negative 
+        }
+ 
+        return true; 
+    }
+};  
+
+struct Scene {
+    std::vector<Sphere> objects;
+    std::vector<Vec3d> light_sources;
+    Color background;
+};
+
+Color trace(const Vec3d &ray_orig, const Vec3d &ray_dir, const Scene &scene){
+    double min_dist = INF;
+    const Sphere *closest_obj = nullptr;
+
+    // calculate closest sphere
+    bool outline = false;
+    for(const Sphere &obj : scene.objects){
+        double t0 = INF, t1 = INF;
+        
+        if(obj.ray_intersection(ray_orig, ray_dir, t0, t1)){
+            if (t0 < min_dist) { 
+                min_dist = t0; 
+                closest_obj = &obj;
+            }
+        }
+    }
+
+
+    Vec3d light_pos = scene.light_sources[0];
+    if(closest_obj){
+        Vec3d hit_loc = ray_orig + (ray_dir * min_dist);
+        Vec3d shadow_dir = light_pos - hit_loc;
+        shadow_dir.normalize();
+
+        bool in_shadow = false;
+        for(const Sphere &obj : scene.objects){
+            double dist;
+            bool outline = false;
+            Vec3d tmp;
+            if(obj.ray_intersection(hit_loc, shadow_dir, dist, dist)){
+                in_shadow = true;
+            }
+        }
+        
+        if(in_shadow){
+            return black;
+        } else {
+            return closest_obj->material.color;
+        }
+    }
+    return background;
+}
+
+#include "writebmp.h"
+
+const int width = 1080, height = 720;
+Color pixels[width * height];
+void makeppm(const std::string &filepath, int w, int h){
+    std::ofstream img{filepath};
+    img << "P3" << std::endl;
+    img << w << " " << h << std::endl;
+    img << "255" << std::endl;
+
+    for(int y = 0; y < h; y++){
+        for(int x = 0; x < w; x++){
+            Color c = pixels[x + y * w];
+            img << c.r() << " " << c.g() << " " << c.b() << std::endl;
+        }
+    }
+}
+
+void render(const std::string &filepath, const Scene &scene){
+    double inv_width = 1 / double(width), inv_height = 1 / double(height); 
+    double fov = 45, aspectratio = width / double(height); 
+    double angle = tan(3.141592 * 0.5 * fov / 180.0); 
+    // Trace rays
+    for(int y = 0; y < height; y++){
+        for(int x = 0; x < width; x++){ 
+            double xx = (2 * ((x + 0.5) * inv_width) - 1) * angle * aspectratio; 
+            double yy = (1 - 2 * ((y + 0.5) * inv_height)) * angle; 
+            Vec3d raydir(xx, yy, -1); 
+
+            raydir.normalize();
+
+            Color c = trace(Vec3d{}, raydir, scene);
+            pixels[x + y * width] = c;
+        } 
+    }
+
+    drawbmp(filepath.c_str(), width, height, pixels); 
+}
+
+int main(){
+    Scene scene;
+
+    Material r = {red};
+    Material g = {green};
+    Material b = {blue};
+    Material floor = {{69, 37, 80}};
+
+    scene.objects.push_back(Sphere({ 0.0, -10004, -20}, 10000.0, floor)); 
+    scene.objects.push_back(Sphere({ 0.0,      0, -20},     4.0, r)); 
+    scene.objects.push_back(Sphere({ 0.0,      5, -35},     4.0, g)); 
+    scene.objects.push_back(Sphere({ 5.0,     2, -15},     2.0, b)); 
+    scene.objects.push_back(Sphere({ 5.0,      0, -25},     3.0, r)); 
+    scene.objects.push_back(Sphere({-5.5,      0, -20},     0.5, g)); 
+    
+    scene.light_sources.push_back({-10,     15, -15}); 
+    // light
+    // for(double i = 0; i < 20; i += 0.1){
+        // scene.light_sources[0].x() = i;
+        render(std::string("raycast.bmp"), scene);
+    // }
+}
+
---
+++ writebmp.h
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "png.h"
+
+void drawbmp (const char *filename, int WIDTH, int HEIGHT, Color *pixels) {
+
+unsigned int headers[13];
+FILE * outfile;
+int extrabytes;
+int paddedsize;
+int x; int y; int n;
+int red, green, blue;
+
+extrabytes = 4 - ((WIDTH * 3) % 4);                 // How many bytes of padding to add to each
+                                                    // horizontal line - the size of which must
+                                                    // be a multiple of 4 bytes.
+if (extrabytes == 4)
+   extrabytes = 0;
+
+paddedsize = ((WIDTH * 3) + extrabytes) * HEIGHT;
+
+// Headers...
+// Note that the "BM" identifier in bytes 0 and 1 is NOT included in these "headers".
+
+headers[0]  = paddedsize + 54;      // bfSize (whole file size)
+headers[1]  = 0;                    // bfReserved (both)
+headers[2]  = 54;                   // bfOffbits
+headers[3]  = 40;                   // biSize
+headers[4]  = WIDTH;  // biWidth
+headers[5]  = HEIGHT; // biHeight
+
+// Would have biPlanes and biBitCount in position 6, but they're shorts.
+// It's easier to write them out separately (see below) than pretend
+// they're a single int, especially with endian issues...
+
+headers[7]  = 0;                    // biCompression
+headers[8]  = paddedsize;           // biSizeImage
+headers[9]  = 0;                    // biXPelsPerMeter
+headers[10] = 0;                    // biYPelsPerMeter
+headers[11] = 0;                    // biClrUsed
+headers[12] = 0;                    // biClrImportant
+
+outfile = fopen(filename, "wb");
+
+//
+// Headers begin...
+// When printing ints and shorts, we write out 1 character at a time to avoid endian issues.
+//
+
+fprintf(outfile, "BM");
+
+for (n = 0; n <= 5; n++)
+{
+   fprintf(outfile, "%c", headers[n] & 0x000000FF);
+   fprintf(outfile, "%c", (headers[n] & 0x0000FF00) >> 8);
+   fprintf(outfile, "%c", (headers[n] & 0x00FF0000) >> 16);
+   fprintf(outfile, "%c", (headers[n] & (unsigned int) 0xFF000000) >> 24);
+}
+
+// These next 4 characters are for the biPlanes and biBitCount fields.
+
+fprintf(outfile, "%c", 1);
+fprintf(outfile, "%c", 0);
+fprintf(outfile, "%c", 24);
+fprintf(outfile, "%c", 0);
+
+for (n = 7; n <= 12; n++)
+{
+   fprintf(outfile, "%c", headers[n] & 0x000000FF);
+   fprintf(outfile, "%c", (headers[n] & 0x0000FF00) >> 8);
+   fprintf(outfile, "%c", (headers[n] & 0x00FF0000) >> 16);
+   fprintf(outfile, "%c", (headers[n] & (unsigned int) 0xFF000000) >> 24);
+}
+
+//
+// Headers done, now write the data...
+//
+
+for (y = HEIGHT - 1; y >= 0; y--)     // BMP image format is written from bottom to top...
+{
+   for (x = 0; x <= WIDTH - 1; x++)
+   {
+
+      red = pixels[x + y * WIDTH].r();
+      green = pixels[x + y * WIDTH].g();
+      blue = pixels[x + y * WIDTH].b();
+
+      if (red > 255) red = 255; if (red < 0) red = 0;
+      if (green > 255) green = 255; if (green < 0) green = 0;
+      if (blue > 255) blue = 255; if (blue < 0) blue = 0;
+
+      // Also, it's written in (b,g,r) format...
+
+      fprintf(outfile, "%c", blue);
+      fprintf(outfile, "%c", green);
+      fprintf(outfile, "%c", red);
+   }
+   if (extrabytes)      // See above - BMP lines must be of lengths divisible by 4.
+   {
+      for (n = 1; n <= extrabytes; n++)
+      {
+         fprintf(outfile, "%c", 0);
+      }
+   }
+}
+
+fclose(outfile);
+return;
+}
